33cbe0a6831216d42ccc4ca71642fba25e71d8f5
vnk8071 <nguyenkhoi8071@gmail.com>
2021-11-06 10:36:56 +0700

    ADD rename function

---
+++ src/rename_format.py
@@ -0,0 +1,28 @@
+import os
+
+# Params
+DATA_PATH = ''  # ./data/NO_INCORRECT_MASK.v1-nim-flipmosaic.yolov5pytorch
+TYPE_AUGMENTATION = ''  # Ex: MOSAIC
+
+
+def main():
+    print('Processing...')
+    images = os.listdir(DATA_PATH + '/train/images')
+    for image_aug in images:
+        old_path = os.path.join(DATA_PATH + '/train/images', image_aug)
+        new_name = image_aug.replace('jpg.rf.', TYPE_AUGMENTATION + '_')
+        new_path = os.path.join(DATA_PATH + '/train/images', new_name)
+        os.rename(old_path, new_path)
+    print('✅ DONE with images')
+
+    labels = os.listdir(DATA_PATH + '/train/labels')
+    for label_aug in labels:
+        old_path = os.path.join(DATA_PATH + '/train/labels', label_aug)
+        new_name = label_aug.replace('jpg.rf.', TYPE_AUGMENTATION + '_')
+        new_path = os.path.join(DATA_PATH + '/train/labels', new_name)
+        os.rename(old_path, new_path)
+    print('✅ DONE with labels')
+
+
+if __name__ == '__main__':
+    main()
